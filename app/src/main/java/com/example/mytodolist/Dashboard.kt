d6f2f06d6130caef74a1c124c50f9c9a6f56f469
Ryunosuke Karimata <jpnryunosuke@gmail.com>
2019-05-14 06:45:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mytodolist/Dashboard.kt
@@ -0,0 +1,145 @@
+package com.example.mytodolist
+
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.TextView
+import com.example.mytodolist.DTO.ToDo
+import kotlinx.android.synthetic.main.activity_dashboard.*
+
+class Dashboard : AppCompatActivity() {
+
+    lateinit var dbHandler: DBHandler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        setSupportActionBar(item_toolbar)
+        title = "タスク一覧"
+        dbHandler = DBHandler(this)
+        rv_dashboard.layoutManager = LinearLayoutManager(this)
+
+        fab_dashboard.setOnClickListener {
+            val dialog = AlertDialog.Builder(this)
+            dialog.setTitle("タスクを追加する")
+            val view = layoutInflater.inflate(R.layout.dialog_dashboard, null)
+            val toDoName = view.findViewById<EditText>(R.id.ev_todo)
+            dialog.setView(view)
+            dialog.setPositiveButton("追加") { _: DialogInterface, _: Int ->
+                if (toDoName.text.isNotEmpty()) {
+                    val toDo = ToDo()
+                    toDo.name = toDoName.text.toString()
+                    dbHandler.addToDo(toDo)
+                    refreshList()
+                }
+            }
+            dialog.setNegativeButton("やめる") { _: DialogInterface, _: Int ->
+
+            }
+            dialog.show()
+        }
+
+    }
+
+    fun updateToDo(toDo: ToDo){
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("タスク名変更")
+        val view = layoutInflater.inflate(R.layout.dialog_dashboard, null)
+        val toDoName = view.findViewById<EditText>(R.id.ev_todo)
+        toDoName.setText(toDo.name)
+        dialog.setView(view)
+        dialog.setPositiveButton("変更") { _: DialogInterface, _: Int ->
+            if (toDoName.text.isNotEmpty()) {
+                toDo.name = toDoName.text.toString()
+                dbHandler.updateToDo(toDo)
+                refreshList()
+            }
+        }
+        dialog.setNegativeButton("やめる") { _: DialogInterface, _: Int ->
+
+        }
+        dialog.show()
+    }
+
+
+    override fun onResume() {
+        refreshList()
+        super.onResume()
+    }
+
+    private fun refreshList(){
+        rv_dashboard.adapter = DashboardAdapter(this,dbHandler.getToDos())
+    }
+
+
+    class DashboardAdapter(val activity: Dashboard, val list: MutableList<ToDo>) :
+        RecyclerView.Adapter<DashboardAdapter.ViewHolder>() {
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(activity).inflate(R.layout.rv_child_dashboard, p0, false))
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, p1: Int) {
+            holder.toDoName.text = list[p1].name
+
+            holder.toDoName.setOnClickListener{
+                val intent = Intent(activity,ItemActivity::class.java)
+                intent.putExtra(INTENT_TODO_ID,list[p1].id)
+                intent.putExtra(INTENT_TODO_NAME,list[p1].name)
+                activity.startActivity(intent)
+            }
+
+            holder.menu.setOnClickListener{
+                val popup = PopupMenu(activity,holder.menu)
+                popup.inflate(R.menu.dashboard_child)
+                popup.setOnMenuItemClickListener {
+
+                    when(it.itemId){
+                        R.id.menu_edit->{
+                            activity.updateToDo(list[p1])
+                        }
+                        R.id.menu_delete->{
+                            val dialog = AlertDialog.Builder(activity)
+                            dialog.setTitle("タスクの削除")
+                            dialog.setMessage("本当に削除しますか??")
+                            dialog.setPositiveButton("削除する") { dialog: DialogInterface?, _: Int ->
+                                activity.dbHandler.deleteToDo(list[p1].id)
+                                activity.refreshList()
+                            }
+                            dialog.setNegativeButton("やめる") { dialog: DialogInterface?, _: Int ->
+
+                            }
+                            dialog.show()
+                        }
+                        R.id.menu_mark_as_completed->{
+                            activity.dbHandler.updateToDoItemCompletedStatus(list[p1].id,true)
+                        }
+                        R.id.menu_reset->{
+                            activity.dbHandler.updateToDoItemCompletedStatus(list[p1].id,false)
+                        }
+                    }
+                    true
+                }
+                popup.show()
+            }
+        }
+
+        class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+            val toDoName: TextView = v.findViewById(R.id.tv_todo_name)
+            val menu : ImageView = v.findViewById(R.id.iv_menu)
+        }
+    }
+}
